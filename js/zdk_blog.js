d4f1a7c8025dd5ff001e62731c619b8c0edcefdb
zdkdchao <zdkdchao@gmail.com>
2019-03-30 15:59:18 +0800

    换仓库，不用web项目装博客文件了

---
+++ js/zdk_blog.js
@@ -0,0 +1,13 @@
+$(function () {
+    $("#article1").click(function () {
+        //window.location.href = "html/容器.htm";
+        window.open("html/容器.htm","_blank");
+    });
+    $("#article2").click(function () {
+        //window.location.href = "html/blog.html";
+        window.open("html/blog.html","_blank");
+    });
+    $("#cwoc").click(function () {
+        window.open("html/confusedWordOfSoftware.htm","_blank");
+    })
+})
