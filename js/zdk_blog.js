ac7cbc1542d60a5695ab9482066a06fe39c91b35
zdkdchao <zdkdchao@gmail.com>
2019-10-13 19:35:12 +0800

    pagerank

--- js/zdk_blog.js
+++ js/zdk_blog.js
@@ -1,14 +1,18 @@
 $(function () {
-    $("#article1").click(function () {
-        //window.location.href = "html/容器.htm";
-        window.open("html/web中的分页研究.htm","_blank");
-    });
-    $("#article2").click(function () {
-        //window.location.href = "html/blog.html";
-        window.open("html/blog.html","_blank");
-    });
-    $("#cwoc").click(function () {
-        window.open("html/confusedWordOfSoftware.htm","_blank");
+    const id2UrlMap = new Map();
+    id2UrlMap.set("article1","html/web中的分页研究.htm")
+    id2UrlMap.set("article2","html/blog.html")
+    id2UrlMap.set("cwoc","html/confusedWordOfSoftware.htm")
+    id2UrlMap.set("articlePR","html/PageRank/pagerank.html")
+    /*
+    点击viewDetails按钮时的业务
+     */
+    const viewDetails = document.querySelectorAll(".viewDetails");
+    viewDetails.forEach((viewDetail) => {
+        viewDetail.onclick = (event) => {
+            id2UrlMap.forEach((v,k) => {
+                if (event.target.id === k) window.open(v,"_blank");
+            })
+        }
     })
-
 })
