05c59bae01156202d87d3737d4e0dedf0a170b14
zdkdchao <zdkdchao@gmail.com>
2019-10-20 16:49:31 +0800

    前端框架技术栈的选择

--- js/zdk_blog.js
+++ js/zdk_blog.js
@@ -5,6 +5,8 @@ $(function () {
     id2UrlMap.set("cwoc","html/confusedWordOfSoftware.htm")
     id2UrlMap.set("articlePR","html/PageRank/pagerank.html")
     id2UrlMap.set("articleDateHandle","html/java中的时间处理/java中的时间处理.html")
+    id2UrlMap.set("articleFrontSel","html\\前端框架技术栈的选择\\前端框架技术栈的选择.html")
+
     /*
     点击viewDetails按钮时的业务
      */
