ef6bd7cb005a7fbbe361558afc99fece4fa1d24a
zdkdchao <zdkdchao@gmail.com>
2019-11-16 17:09:29 +0800

    ru

--- js/zdk_blog.js
+++ js/zdk_blog.js
@@ -13,6 +13,7 @@ $(function () {
     id2UrlMap.set("articleHadoopHA","html/hadoopHA的搭建_0/hadoopHA的搭建_0.html")
     id2UrlMap.set("articleHadoopBuild","html/hadoop集群非HA非安全模式的搭建_0/hadoop集群非HA非安全模式的搭建_0.html")
     id2UrlMap.set("articleVMStandard","html/克隆模板虚拟机的设置_0/克隆模板虚拟机的设置_0.html")
+    id2UrlMap.set("articleHive","html/hive的窗口函数、排名函数、lve/hive的窗口函数、排名函数、lve.html")
 
     /*
     点击viewDetails按钮时的业务
