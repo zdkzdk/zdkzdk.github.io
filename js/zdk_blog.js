cb7fdf9f043c3c9d4a1b263d5bc9ee729ba761dd
zdkdchao <zdkdchao@gmail.com>
2020-01-02 13:09:38 +0800

    kafka同SparkStreaming的整合

--- js/zdk_blog.js
+++ js/zdk_blog.js
@@ -21,6 +21,7 @@ $(function () {
     id2UrlMap.set("articleSpark3","html/spark源码分析-资源调度/spark源码分析-资源调度.html")
     id2UrlMap.set("articleSpark4","html/spark源码分析-任务调度/spark源码分析-任务调度.html")
     id2UrlMap.set("articleKafkaSparkStreaming","html/kafka同SparkStreaming的整合/kafka同SparkStreaming的整合.html")
+    id2UrlMap.set("articleCoding","html/手写代码练习/手写代码练习.html")
 
     /*
     点击viewDetails按钮时的业务
