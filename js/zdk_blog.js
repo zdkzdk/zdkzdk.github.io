0cbfc358ca5701b9a6c600da2621eff88cdbfb64
zdkzdk <42910604+zdkzdk@users.noreply.github.com>
2022-03-26 15:54:32 +0800

    Update zdk_blog.js

--- js/zdk_blog.js
+++ js/zdk_blog.js
@@ -1,6 +1,6 @@
 $(function () {
     const id2UrlMap = new Map();
-    id2UrlMap.set("other1","html/曝光一个废物无耻骗子：财信证券南宁分理处的戴剑波/曝光一个废物无耻骗子：财信证券南宁分理处的戴剑波.html")
+    id2UrlMap.set("other1","html/other1/other1.html")
     id2UrlMap.set("article1","html/web中的分页研究.htm")
     id2UrlMap.set("article2","html/blog.html")
     id2UrlMap.set("cwoc","html/confusedWordOfSoftware.htm")
